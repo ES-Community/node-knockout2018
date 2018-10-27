8901cf002ecd60ee21912011cdbb2cad91cd5cfc
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-10-27 19:54:35 +0200

    Update Home

--- ressources/src/views/Home.vue
+++ ressources/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <h1>This is your bookmark manager 🚀</h1>
+    <br>
+    <p>We create this webapp to allow you to manage your bookmarks easily <br>
+    We did it for the <a href="https://www.nodeknockout.com">Node Knockout</a> hackaton</p>
+    <br>
+    <p>Insert GIF here</p>
   </div>
 </template>
 
