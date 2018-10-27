2fef327d331c60b23c42e95264e7622783dfa7ba
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-10-27 19:30:52 +0200

    Add Navbar

--- ressources/src/App.vue
+++ ressources/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
 
 <style>
 #app {
@@ -26,6 +34,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #2701d5;
 }
 </style>
