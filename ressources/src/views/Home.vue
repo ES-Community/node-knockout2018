fa75e73632a5456a7670e2b67d976e98631899cd
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-10-28 23:20:10 +0100

    Fix typo

--- ressources/src/views/Home.vue
+++ ressources/src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <v-container id="home">
-    <img width="600" src="/bookman.png" alt="Bookman">
+    <v-layout row wrap>
+      <v-img width="600" src="/bookman.png" alt="Bookman logo"></v-img>
+    </v-layout>
     <br><br>
     <h1>This is your bookmark manager 🚀</h1>
     <br>
     <p>We create this webapp to allow you to manage your bookmarks easily <br>
-      We also did it for the Node Knockout hackaton</p>
+      We also did it for the <a href="https://www.nodeknockout.com/entries/76-es-community-junior/vote">Node Knockout</a> hackathon</p>
     <v-btn flat to="/login" color="info">Login</v-btn>
     <v-btn flat to="/signup" color="info">Signup</v-btn>
     <div id="hackbit-vote-widget"></div>
@@ -30,6 +32,8 @@
   text-align: center;
   padding: 7%;
   padding-bottom: 0%;
+  display: inline-block;
 }
+
 </style>
 
