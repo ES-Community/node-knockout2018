b3adb3bf9c59a704c52f629c615fbb585f15304b
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-10-28 22:42:39 +0100

    Add style

--- ressources/src/views/Home.vue
+++ ressources/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container id="home">
-    <img src="/bookman.png" alt="Bookman">
+    <img width="600" src="/bookman.png" alt="Bookman">
+    <br><br>
     <h1>This is your bookmark manager 🚀</h1>
     <br>
     <p>We create this webapp to allow you to manage your bookmarks easily <br>
       We also did it for the Node Knockout hackaton</p>
     <v-btn flat to="/login" color="info">Login</v-btn>
     <v-btn flat to="/signup" color="info">Signup</v-btn>
-    <br>
     <div id="hackbit-vote-widget"></div>
   </v-container>
 </template>
@@ -28,7 +28,8 @@
 <style>
 #home {
   text-align: center;
-  padding: 10%;
+  padding: 7%;
+  padding-bottom: 0%;
 }
 </style>
 
